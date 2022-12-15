5eee7298260178a37dbe7881b859fd122cc2a46b
marcelamchdo <arqmrcl@gmail.com>
2022-12-15 17:16:55 -0300

    correct lint 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,12 @@
-db.produtos.find({}, {
-  nome: 1, vendidos: 1, _id: 0 
+db.produtos.find(
+  {},
+  {
+  nome: 1, 
+  vendidos: 1,
+  _id: 0,
   },
 ).sort(
   {
-    vendidos: 1
+    vendidos: 1,
   },
 );
